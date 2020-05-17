add18b37da694732613c7a69cf5c25b588189aab
Teofilo Chichume <nhatofo@live.com.pt>
2020-05-17 05:12:03 +0200

    Initial commit on first deploy

--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@ export AUTH0_DOMAIN='nhatofo.auth0.com'
 export API_AUDIENCE='casting_agency'
 export ALGORITHMS=['RS256']
 export DATABASE_URL="postgres://pxwduiqyoswvph:b1bcb67d4595e3f7172d829ca6fbdd4420a7147c89ec5c287b23afc110b09231@ec2-34-204-22-76.compute-1.amazonaws.com:5432/dacae3ho9cob5f"
+ 
